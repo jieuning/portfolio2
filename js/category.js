1f03f46bee967dfe911e253cc9256c1de509fa1e
jieun <balllling92@gmail.com>
2023-06-16 17:28:43 +0900

    Feat: 스킬 탭 기능 추가

--- js/category.js
+++ js/category.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".cate-btn").on("click", () => {
+  $(".cate-btn").click(function() {
     // 카테고리 열림/닫힘
     $(".category").toggleClass("open");
 
@@ -8,18 +8,17 @@ $(document).ready(function () {
     $(".cate-btn span").toggleClass("toggle2");
 
     // 카테고리 메뉴 등장 애니메이션
-    let menus = $(".menu li").length;
+    let menus = $("#menu li").length;
     let cateLength = $(".category.open").length
 
     // .category.open 추가일때, .category.open이 제거되었을때
     for (let i = 0; i < menus; i++) {
       if (cateLength == 1) {
-        $(".menu li").eq(i).delay(i * 200).animate({ "left": 0, "opacity": 1 }, 1000, "easeOutBack");
+        $("#menu li").eq(i).delay(i * 200).animate({ "left": 0, "opacity": 1 }, 1000, "easeOutBack");
       }
       else {
-        $(".menu li").eq(i).delay(i * 200).animate({ "left": "100px", "opacity": 0 }, 1000, "easeOutBack");
+        $("#menu li").eq(i).delay(i * 200).animate({ "left": "100px", "opacity": 0 }, 1000, "easeOutBack");
       }
     };
-
   });
 });
--- js/email.js
+++ js/email.js
@@ -1,16 +1,17 @@
 // emailjs 호출
 const sendEmail = () => {
-    let templateParams = {
-      name: document.getElementById('name').value,
-      email: document.getElementById('email').value,
-      message: document.getElementById('message').value,
-    }
-    emailjs.send('service_9xiq6nr', 'template_rl9hnii', templateParams)
-      .then(function (res) {
-        console.log(res);
-        alert("메일이 성공적으로 발송되었습니다.");
-      }, function (error) {
-        console.log(error);
-        alert("메일 전송에 실패했습니다.");
-      })
+  let templateParams = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    message: document.getElementById('message').value,
   };
+
+  emailjs.send('service_9xiq6nr', 'template_rl9hnii', templateParams)
+    .then((res) => {
+      console.log(res);
+      alert("메일이 성공적으로 발송되었습니다.");
+    }, (error) => {
+      console.log(error);
+      alert("메일 전송에 실패했습니다.");
+    });
+};
